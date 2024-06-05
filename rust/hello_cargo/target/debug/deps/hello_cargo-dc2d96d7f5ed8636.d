c7dd1fd60679777f99bbcbab00233e726048b0e7
Andr3wcr0z <aduron2010@gmail.com>
2024-06-04 21:05:16 -0600

    rust updates

---
+++ rust/hello_cargo/target/debug/deps/hello_cargo-dc2d96d7f5ed8636.d
@@ -0,0 +1,5 @@
+/home/andr3wcr0z/learning-projects/rust/hello_cargo/target/debug/deps/hello_cargo-dc2d96d7f5ed8636: src/main.rs
+
+/home/andr3wcr0z/learning-projects/rust/hello_cargo/target/debug/deps/hello_cargo-dc2d96d7f5ed8636.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/andr3wcr0z/learning-projects/rust/hello_cargo/target/debug/hello_cargo: /home/andr3wcr0z/learning-projects/rust/hello_cargo/src/main.rs
